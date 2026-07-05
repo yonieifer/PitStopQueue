c0d8cba5cf4bb988fa4072210ab2c872fd1fc994
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-05 12:26:21 +0300

    move all cars logic to carsMethods.js

---
+++ carsMethods.js
@@ -0,0 +1,37 @@
+const getNextCar = (cars) => {
+    return cars[0]
+}
+
+const getCarByNumber = (cars, num) => {
+    for (const car of cars) {
+        if (car.carNumber === num) return car
+        else {
+            console.log(`car number ${num} not found in the current race.`);
+        }
+    }
+}
+
+
+const carsAlreadyPassed = (cars) => {
+    let fixedCars = 0
+    cars.forEach(car => {
+        if (car.status === "done") {
+            fixedCars++
+        }
+    });
+    return fixedCars
+}
+
+const carFormat = (car) => `Car #${car.carNumber} | Driver: ${car.driverName}`
+
+
+const printWaitingCars = (cars) => {
+    cars.forEach(car => {
+        if (car.status === "waiting") {
+            console.log(`- ${carFormat(car)}`);
+        }
+    })
+}
+
+export {getCarByNumber, getNextCar, carsAlreadyPassed, printWaitingCars, carFormat}
+
